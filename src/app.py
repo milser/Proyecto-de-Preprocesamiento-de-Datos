23246c1cbfe58128c30024644c27e96802987824
milser <ruben2work@gmail.com>
2024-03-25 19:17:33 +0100

    first commit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,18 @@
-from utils import db_connect
-engine = db_connect()
+import os
+import psycopg2
+import pandas as pd
+from dotenv import load_dotenv
 
-# your code here
+# load the .env file variables
+load_dotenv()
+
+# 1) Connect to the database here using the SQLAlchemy's create_engine function
+#con_str = "postgresql://gitpod:postgres:@localhost/milser_pg"
+global engine # This allows us to use a global variable called engine
+# A "connection string" is basically a string containing all database credentials together.
+#connection_string = f"dbname={os.getenv('DB_NAME')} user={os.getenv('DB_USER')} password={os.getenv('DB_PASSWORD')} host={os.getenv('DB_HOST')}"
+
+total_data = pd.read_csv("../data/raw/AB_NYC_2019.csv")
+total_data.head()
+#engine = create_engine(connection_string)
+#Session = sessionmaker(bind=engine,autocommit=False) 
