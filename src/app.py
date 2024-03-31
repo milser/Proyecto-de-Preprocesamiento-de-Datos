a40d58b2f7509494b4d4adbd3228dc2a576f55ba
milser <ruben2work@gmail.com>
2024-03-31 22:25:54 +0200

    sunday commit

--- src/app.py
+++ src/app.py
@@ -12,7 +12,16 @@ global engine # This allows us to use a global variable called engine
 # A "connection string" is basically a string containing all database credentials together.
 #connection_string = f"dbname={os.getenv('DB_NAME')} user={os.getenv('DB_USER')} password={os.getenv('DB_PASSWORD')} host={os.getenv('DB_HOST')}"
 
-total_data = pd.read_csv("../data/raw/AB_NYC_2019.csv")
-total_data.head()
 #engine = create_engine(connection_string)
 #Session = sessionmaker(bind=engine,autocommit=False) 
+
+# Ruta del directorio del script
+script_dir = os.path.dirname(__file__)  # Obtiene la ruta del directorio actual del script
+# Construir la ruta al archivo CSV
+relative_path = os.path.join("..","..","Proyecto-de-Preprocesamiento-de-Datos", "data", "raw", "AB_NYC_2019.csv")
+absolute_path = os.path.abspath(os.path.join(script_dir, relative_path))
+#path=("../../Proyecto-de-Preprocesamiento-de-Datos/data/raw/AB_NYC_2019")
+
+# Leer el archivo CSV
+total_data = pd.read_csv(absolute_path)
+print(total_data.head())
